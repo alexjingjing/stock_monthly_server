3a6c209ffd343a5529b97a5f1c0504ac95123c1e
liusiming <abanmiku@hotmail.com>
2018-02-11 17:29:55 +0800

    update

--- app/controller/stock.py
+++ app/controller/stock.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from flask import Blueprint, request, jsonify, redirect
+from pymongo import DESCENDING, ASCENDING
 from app import stockItem
 import re
 
@@ -10,6 +11,8 @@ stock = Blueprint('stock', __name__)
 def get_stock_list():
     time_str = request.args['time']
     time_re = re.compile('\d{6}' + time_str)
-    for s in stockItem.find({"date": time_re}):
-        print(s)
-    return 'yeah'
+    s_list = []
+    for s in stockItem.find({"date": time_re}, {"_id": 0, "createTime": 0})\
+            .sort([("createTime", ASCENDING)]):
+        s_list.append(s)
+    return jsonify({"errorCode": 0, "stockList": s_list})
